efc313a196831d108ea1d350cf7a5f335416166d
sernap97 <sebas.ssp97@gmail.com>
2019-01-21 00:43:39 -0600

    Exercise 1-23 works for deleting comments, doesn't take into account strings with the comment symbols

---
+++ labs/lab0.1/ex1-23.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main() {
+    FILE *fp;
+    int c,
+        checking = 1;
+    fpos_t pos,
+           pos2;
+
+    fp = fopen("test.c", "r+");
+    if(fp) {
+        while(!feof(fp)) {
+            fgetpos(fp, &pos);
+            c = getc(fp);
+            if(c == '/') {
+                c = getc(fp);
+                switch(c) {
+                    case '/':
+                        fsetpos(fp, &pos);
+                        while(ungetc(getc(fp), fp) != '\n' && !feof(fp)) {
+                            fputc(' ', fp);
+                        }
+                        fsetpos(fp, &pos);
+                        fputc(' ', fp);
+                        break;
+                    case '*':
+                        do {
+                            while(ungetc(getc(fp), fp) != '*') {
+                                fputc(' ', fp);
+                            }
+                            fgetpos(fp, &pos2);
+                            getc(fp);
+                            if(ungetc(getc(fp), fp) == '/') {
+                                fsetpos(fp, &pos);
+                                fputc(' ', fp);
+                                fputc(' ', fp);
+                                checking = 0;
+                            }
+                            else {
+                                fsetpos(fp, &pos2);
+                                fputc(' ', fp);
+                                checking = 1;
+                            }
+                        } while(checking);
+                        fsetpos(fp, &pos2);
+                        fputc(' ', fp);
+                        fputc(' ', fp);
+                        break;
+                }
+            }
+        }
+    fclose(fp);
+    }
+}
