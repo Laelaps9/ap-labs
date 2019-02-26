d4e2a7efacdb6fb117cc53b3cc7596cc35e03c9b
sernap97 <sebas.ssp97@gmail.com>
2019-02-26 16:43:54 -0600

    Functional, formats the results into a document

--- labs/lab2.3/cross-ref.c
+++ labs/lab2.3/cross-ref.c
@@ -3,74 +3,92 @@
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <ctype.h>
 
 #define MAX_WORD_COUNT 1000
 #define MAX_WORD_SIZE 50
+#define DICTIONARY_SIZE 10000
+#define MAX_WORDS 32000
+#define REPORT_FILE "words.txt"
 
 // Struct that will act as node for a linked list of words.
-typedef struct node {
+typedef struct word {
     // Value of the word itself
-    char *word;
+    char word[MAX_WORD_SIZE];
     // Array of ints to store the lines in which the word was found
-    int *lines;
+    int lines[MAX_WORD_COUNT];
     // Iterations of the words
     int count;
-    // Pointer to the next node
-    struct node *next;
-} node_t;
+}word;
 
+struct word word_dict[DICTIONARY_SIZE];
 
-void printlist(node_t *head) {
-    node_t *current = head;
-
-    while(current != NULL) {
-        printf("[%s] -> ", current->word);
-        current = current->next;
-    }
-    printf("NULL\n");
-}
-
-node_t * hasWord(node_t *head, char *word) {
-    node_t *current = head;
-    while(current != NULL) {
-        if(strcmp(current->word, word) == 0) {
-            return current;
+int hasWord(char *word) {
+    for(int i = 0; word_dict[i].word[0] != '\0'; i++) {
+        if(strcmp(word_dict[i].word, word) == 0) {
+            return i;
         }
     }
-    return NULL;
+    return -1;
 }
 
-void push(node_t **head, char *word, int currentLine) {
-
-    if(hasWord(*head, word) == NULL) {
+void push(char *word, int currentLine) {
+    int index = 0;
+    char *copy = (char *)calloc(sizeof(char), MAX_WORD_SIZE);
+    strcpy(copy, word);
+    if((index = hasWord(word)) == -1) {
         // If word isn't found, create a node with that word
-        node_t *node = malloc(sizeof(node_t));
-        printf("No2\n");
-        node->next = *head;
-        node->word = word;
-        node->lines = (int *)calloc(1000, sizeof(int));
-        node->lines[0] = currentLine;
-        node->count = 1;
-        *head = node;
+        int i = 0;
+        while(word_dict[i].word[0] != '\0') {
+            i++;
+        }
+        strcpy(word_dict[i].word, copy);
+        word_dict[i].lines[0] = currentLine;
+        word_dict[i].count = 1;
     }
 
     // If word is found, add the new line it was found on and increase word iteration
     else {
-//        node->lines[node->count] = currentLine;
-//        node->count++;
-        printf("Found\n");
+        word_dict[index].lines[word_dict[index].count] = currentLine;
+        word_dict[index].count++;
     }
 }
 
+// Check to see if the word is
 int wordCheck(char *word) {
     int size = 0;
+    //printf("%s\n", word);
     while(word[size] != '\0') {
         size++;
     }
-    if(size <= 1) {
+    if(size <= 2) {
         return 0;
     }
-    return 1;
+
+    // List of noise words
+    if(strcmp(word, "and") == 0 || strcmp(word, "the") == 0 || strcmp(word, "for") == 0 || strcmp(word, "from") == 0 || strcmp(word, "but") == 0 || strcmp(word, "with") == 0 || strcmp(word, "like") == 0) {
+        return 0;
+    }
+    else {
+        return 1;
+    }
+}
+
+void write_doc() {
+    char str[5];
+    char tmp[5];
+    tmp[0] = '\t';
+    int report = open(REPORT_FILE, O_RDWR | O_CREAT, 0666);
+    for(int i = 0; word_dict[i].word[0 != '\0']; i++) {
+        write(report, strcat(word_dict[i].word, ":\n"), strlen(word_dict[i].word) + 2);
+        for(int j = 0; word_dict[i].lines[j] != 0; j++) {
+            sprintf(str, "%d", word_dict[i].lines[j]);
+            write(report, " ", 1);
+            write(report, strcat(str, ", "), strlen(str) + 1);
+        }
+        write(report, "\n", 1);
+    }
+    close(report);
 }
 
 void wordCount(char *file) {
@@ -80,30 +98,28 @@ void wordCount(char *file) {
         file_size = lseek(fd, 0, SEEK_END);
     char data[file_size + 1],
          word[MAX_WORD_SIZE];
-    node_t *head = NULL;
 
     // Go back to the beginning of the file
     lseek(fd, 0, SEEK_SET);
     read(fd, data, file_size + 1);
     close(fd);
 
-    for(int i = 0; i < 100; i++) {
+    for(int i = 0; i < file_size; i++) {
         int j = i;
-
         // Stop when there's a symbol that doesn't belong to a word
         while(data[i] != ' ' && data[i] != '\n' && data[i] != ',' && data[i] != '.' && data[i] != ';' && data[i] != '"') {
-            word[i - j] = data[i];
+            word[i - j] = tolower(data[i]);
             i++;
         }
         if(wordCheck(word)) {
-            push(&head, word, currentLine);
+            push(word, currentLine);
             memset(word, 0, MAX_WORD_SIZE);
         }
         if(data[i] == '\n') {
             currentLine++;
         }
     }
-    printlist(head);
+    write_doc();
 }
 
 int main(int argc, char **argv) {
