16471442657f9e051b64707696090e7b9091d5fc
sernap97 <sebas.ssp97@gmail.com>
2019-03-28 15:47:15 -0600

    Fixed stack smashing problem when getting number of threads. Program now retrieves name, state, pid, ppid, and threads.

--- challenges/chal3/mytop/mytop.c
+++ challenges/chal3/mytop/mytop.c
@@ -2,7 +2,7 @@
 #include <dirent.h>
 #include <string.h>
 
-#define INFO_LENGTH 30
+#define INFO_LENGTH 40
 #define MAX_PROCESSES 1000
 
 void clear();
@@ -24,6 +24,7 @@ struct process processes[MAX_PROCESSES];
 int main(){
 	// Place your magic here
 	readProcesses();
+	printf("END 2\n");
 	printProcesses();
 	clear();
 	return 0;
@@ -57,7 +58,13 @@ int readProcesses() {
 					case 0:
 						do {
 							c = getc(fp);
-						} while(c != 'N');
+						} while(c != 'N' && c != EOF);
+
+						if(c == EOF) {
+							fseek(fp, 0L, SEEK_SET);
+							strcpy(processes[procNum].name, "");
+							break;
+						}
 
 						// Checking to see if what was found was the tag "Name"
 						if(getc(fp) == 'a' && getc(fp) == 'm' && getc(fp) == 'e' && getc(fp) == ':') {
@@ -70,32 +77,160 @@ int readProcesses() {
 								c = getc(fp);
 							}
 							strcpy(processes[procNum].name, info);
+
+							// Go back to the beginning of the file
+							fseek(fp, 0L, SEEK_SET);
 						}
 						else {
-							dataNum = 0;
+							dataNum = -1;
 						}
 						break;
 						case 1:
 							do {
 								c = getc(fp);
-							} while(c != 'P');
+							} while(c != 'S' && c != EOF);
 
-							// Checking to see if what was found was the tag "Pid"
-							if(getc(fp) == 'i' && getc(fp) == 'd' && getc(fp) == ':') {
+							if(c == EOF) {
+								fseek(fp, 0L, SEEK_SET);
+								break;
+							}
+
+							// Checking to see if what was found was the tag "State"
+							if(getc(fp) == 't' && getc(fp) == 'a' && getc(fp) == 't' && getc(fp) == 'e' && getc(fp) == ':') {
 								do {
 									c = getc(fp);
-								} while(c == ' ');
+								} while(c != '(');
 
-								for(int j = 0; c != '\n'; j++) {
+								c = getc(fp);
+
+								for(int j = 0; c != ')'; j++) {
 									info[j] = c;
 									c = getc(fp);
 								}
-								strcpy(processes[procNum].pid, info);
+								strcpy(processes[procNum].state, info);
+								fseek(fp, 0L, SEEK_SET);
 							}
 							else {
-								dataNum = 1;
+								dataNum = 0;
 							}
 							break;
+							case 2:
+								do {
+									c = getc(fp);
+								} while(c != 'P' && c != EOF);
+
+								if(c == EOF) {
+									fseek(fp, 0L, SEEK_SET);
+			//						strcpy(processes[procNum].pid, "");
+									break;
+								}
+
+								// Checking to see if what was found was the tag "Pid"
+								if(getc(fp) == 'i' && getc(fp) == 'd' && getc(fp) == ':') {
+									do {
+										c = getc(fp);
+									} while(c == ' ');
+
+									for(int j = 0; c != '\n'; j++) {
+										info[j] = c;
+										c = getc(fp);
+									}
+									strcpy(processes[procNum].pid, info);
+
+									fseek(fp, 0L, SEEK_SET);
+
+								}
+								else {
+									dataNum = 1;
+								}
+								break;
+							case 3:
+								do {
+									c = getc(fp);
+								} while(c != 'P' && c != EOF);
+
+								if(c == EOF) {
+									fseek(fp, 0L, SEEK_SET);
+									break;
+								}
+
+								// Checking to see if what was found was the tag "PPid"
+								if(getc(fp) == 'P' && getc(fp) == 'i' && getc(fp) == 'd' && getc(fp) == ':') {
+									do {
+										c = getc(fp);
+									} while(c == ' ');
+
+									for(int j = 0; c != '\n'; j++) {
+										info[j] = c;
+										c = getc(fp);
+									}
+									strcpy(processes[procNum].ppid, info);
+									fseek(fp, 0L, SEEK_SET);
+								}
+								else {
+									dataNum = 2;
+								}
+								break;
+								case 4:
+									do {
+										c = getc(fp);
+									} while(c != 'V' && c != EOF);
+									if(c == EOF) {
+										fseek(fp, 0L, SEEK_SET);
+										break;
+									}
+
+									// Checking to see if what was found was the tag "VmRSS"
+									if(getc(fp) == 'm' && getc(fp) == 'R' && getc(fp) == 'S' && getc(fp) == 'S' && getc(fp) == ':') {
+										do {
+											c = getc(fp);
+										} while(c == ' ');
+
+										for(int j = 0; c != ' '; j++) {
+											info[j] = c;
+											c = getc(fp);
+											if(c == ' ') {
+											}
+										}
+										strcpy(processes[procNum].memory, info);
+
+										fseek(fp, 0L, SEEK_SET);
+									}
+									else {
+										dataNum = 3;
+									}
+									break;
+									case 5:
+										do {
+											c = getc(fp);
+										} while(c != 'T' && c != EOF);
+
+										if(c == EOF) {
+											fseek(fp, 0L, SEEK_SET);
+											break;
+										}
+
+										// Checking to see if what was found was the tag "Threads"
+										if(getc(fp) == 'h' && getc(fp) == 'r' && getc(fp) == 'e' && getc(fp) == 'a' && getc(fp) == 'd' && getc(fp) == 's' && getc(fp) == ':') {
+
+											do {
+												c = getc(fp);
+											} while(c == ' ');
+
+											if(c == '\n') {
+												break;
+											}
+
+											for(int j = 0; c != '\n'; j++) {
+												info[j] = c;
+												c = getc(fp);
+											}
+											strcpy(processes[procNum].threads, info);
+										}
+										else {
+											dataNum = 4;
+										}
+										break;
 				}
 				memset(info, 0, INFO_LENGTH);
 				dataNum++;
@@ -113,7 +248,7 @@ int readProcesses() {
 int printProcesses() {
 	int i = 0;
 	while(processes[i].name[0] != '\0') {
-		printf("Name: %s - Pid: %s\n", processes[i].name, processes[i].pid);
+		printf("%s :: %s :: %s :: %s :: %s :: %s\n", processes[i].name, processes[i].state, processes[i].pid, processes[i].ppid, processes[i].memory, processes[i].threads);
 		i++;
 	}
 	return 0;
