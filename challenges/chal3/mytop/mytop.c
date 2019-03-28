ae2966eaf80606cb2f3d68f1cc1df827b5735ce6
sernap97 <sebas.ssp97@gmail.com>
2019-03-28 00:35:32 -0600

    Program correctly retrieves Name and Pid of all the processes in the /proc directory and saves them into an array of structs with all the elements to be displayed as attributes.

--- challenges/chal3/mytop/mytop.c
+++ challenges/chal3/mytop/mytop.c
@@ -2,48 +2,120 @@
 #include <dirent.h>
 #include <string.h>
 
+#define INFO_LENGTH 30
+#define MAX_PROCESSES 1000
+
 void clear();
+int readProcesses();
+int printProcesses();
+
+struct process {
+	char name[INFO_LENGTH],
+		 state[INFO_LENGTH],
+		 pid[INFO_LENGTH],
+		 ppid[INFO_LENGTH],
+		 memory[INFO_LENGTH],
+		 threads[INFO_LENGTH],
+		 openFiles[INFO_LENGTH];
+};
+
+struct process processes[MAX_PROCESSES];
 
 int main(){
 	// Place your magic here
+	readProcesses();
+	printProcesses();
+	clear();
+	return 0;
+}
+
+int readProcesses() {
 	DIR *d = opendir("/proc/");
 	struct dirent *dir;
 	FILE *fp;
 	char path[30],
-		 c;
-	int p = 0;
+		 info[INFO_LENGTH];
+	int dataNum,
+		procNum = 0,
+		c;
 
+	memset(info, 0, INFO_LENGTH);
 	strcpy(path, "/proc/");
 	while((dir = readdir(d)) != NULL) {
 
 		// Filters out folders starting with a number
 		if(dir->d_name[0] >=48 && dir->d_name[0] <=57) {
-
+			dataNum = 0;
 			// Add path of directory and status to know which file to enter
 			strcat(path, dir->d_name);
 			strcat(path, "/status");
-			printf("%s\n", path);
 			// Read status file
 			fp = fopen(path, "r");
-			// Find PID
-			while((c = getc(fp)) != 'P') {
-			}
-			// Print PID and PPID
-			while(p < 2) {
-				while((c = getc(fp)) != '\n') {
-					printf("%c", c);
+
+			while(dataNum < 6) {
+				switch(dataNum) {
+					case 0:
+						do {
+							c = getc(fp);
+						} while(c != 'N');
+
+						// Checking to see if what was found was the tag "Name"
+						if(getc(fp) == 'a' && getc(fp) == 'm' && getc(fp) == 'e' && getc(fp) == ':') {
+							do {
+								c = getc(fp);
+							} while(c == ' ');
+
+							for(int j = 0; c != '\n'; j++) {
+								info[j] = c;
+								c = getc(fp);
+							}
+							strcpy(processes[procNum].name, info);
+						}
+						else {
+							dataNum = 0;
+						}
+						break;
+						case 1:
+							do {
+								c = getc(fp);
+							} while(c != 'P');
+
+							// Checking to see if what was found was the tag "Pid"
+							if(getc(fp) == 'i' && getc(fp) == 'd' && getc(fp) == ':') {
+								do {
+									c = getc(fp);
+								} while(c == ' ');
+
+								for(int j = 0; c != '\n'; j++) {
+									info[j] = c;
+									c = getc(fp);
+								}
+								strcpy(processes[procNum].pid, info);
+							}
+							else {
+								dataNum = 1;
+							}
+							break;
 				}
-				printf("\n");
-				p++;
+				memset(info, 0, INFO_LENGTH);
+				dataNum++;
+				c = '\0';
 			}
-			printf("\n");
-			p = 0;
 			strcpy(path, "/proc/");
+			fclose(fp);
+			procNum++;
 		}
-
 	}
+	closedir(d);
+	return 0;
+}
 
-	clear();
+int printProcesses() {
+	int i = 0;
+	while(processes[i].name[0] != '\0') {
+		printf("Name: %s - Pid: %s\n", processes[i].name, processes[i].pid);
+		i++;
+	}
 	return 0;
 }
 
